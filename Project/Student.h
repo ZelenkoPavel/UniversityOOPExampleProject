16e15e02db95705e979766b6b269cd39de984b52
ITSTEP\st <st>
2024-03-26 18:23:28 +0300

    .

---
+++ Project/Student.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Student {
+public:
+	string name;
+	int age;
+	double mark;
+};
---
+++ Project/main.cpp
@@ -0,0 +1,22 @@
+#include "Student.h"
+
+int main() {
+	Student st1;
+	Student st2;
+	Student st3;
+
+	st1.name = "Alex";
+	st1.name = 14;
+	st1.mark = 9;
+
+	st2.name = "Alice";
+	st2.name = 13;
+	st2.mark = 7;
+
+	st3.name = "Potter";
+	st3.name = 15;
+	st3.mark = 10;
+
+
+	return 0;
+}
