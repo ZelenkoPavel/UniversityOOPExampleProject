663264d0678751c6e45224c58f9ef53fcba6853e
ITSTEP\st <st>
2024-03-28 18:40:55 +0300

    .

--- Project/Student.h
+++ Project/Student.h
@@ -12,24 +12,24 @@ public:
 
 
 	// default constructor
-	Student() {
-		//cout << "calling default constructor" << endl;
-		name = "no name";
-		age = 5;
-		mark = 4;
-	}
+	//Student() {
+	//	cout << "calling default constructor" << endl;
+	//	name = "no name";
+	//	age = 5;
+	//	mark = 4;
+	//}
 
 
 	// canonical constructor with parameters (arguments)
 	Student(string nm, int a, double m) {
-		//cout << "calling canonical constructor" << endl;
+		cout << "calling canonical constructor with parameters 1" << endl;
 		name = nm;
 		age = a;
 		mark = m;
 	}
 
 	Student(string nm) {
-		//cout << "calling canonical constructor" << endl;
+		cout << "calling canonical constructor with parameters 2" << endl;
 		name = nm;
 		age = 0;
 		mark = 0;
--- Project/main.cpp
+++ Project/main.cpp
@@ -4,16 +4,37 @@
 using namespace std;
 
 int main() {
+	//int n{ 10 };
+	//int n = 10;
+	//int array[10]{};
 
-	Student st1("Alex", 14, 9);
-	Student st2("Alice", 13, 7);
-	Student st3("Potter", 15, 10);
-	Student st4;
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << array[i] << endl;
+	//}
 
-	cout << st1.convert() << endl;
-	cout << st2.convert() << endl;
-	cout << st3.convert() << endl;
-	cout << st4.convert() << endl;
+	//Student st1("Alex", 14, 9);
+	//Student st2("Alice", 13, 7);
+	//Student st3("Potter", 15, 10);
+	//Student st4;
+	//Student st5{};
+	//Student st6{"Peter"};
+	//Student st7{ "Anna", 13, 7};
+	////Student st8{ "Anna", 13};  // !!! Compiler Error
+	//Student st9 = Student("Alice");
+
+	const int size = 5;
+	Student students[size]{ {"Alice"}, {10}, {"Peter", 14, 9}, {"Peter"}, {9}};
+
+
+	//cout << st1.convert() << endl;
+	//cout << st2.convert() << endl;
+	//cout << st3.convert() << endl;
+	//cout << st4.convert() << endl;
+	//cout << st5.convert() << endl;
+	//cout << st6.convert() << endl;
+	//cout << st7.convert() << endl;
+	////cout << st8.convert() << endl;
 
 	return 0;
 }
