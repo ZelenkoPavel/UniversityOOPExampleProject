47afa957b4941a6bffb781ec4861131ba8459075
ITSTEP\st <st>
2024-03-28 18:51:16 +0300

    destructor (деструктор)

--- Project/Student.h
+++ Project/Student.h
@@ -9,30 +9,45 @@ public:
 	string name;
 	int age;
 	double mark;
+	//double* mark;
 
 
 	// default constructor
-	//Student() {
-	//	cout << "calling default constructor" << endl;
-	//	name = "no name";
-	//	age = 5;
-	//	mark = 4;
-	//}
+	Student() {
+		cout << "calling default constructor" << endl;
+		name = "no name";
+		age = 5;
+		mark = 4;
+		//*mark = new double[5];
+	}
 
 
 	// canonical constructor with parameters (arguments)
 	Student(string nm, int a, double m) {
-		cout << "calling canonical constructor with parameters 1" << endl;
+		cout << "calling canonical constructor" << endl;
 		name = nm;
 		age = a;
 		mark = m;
 	}
 
 	Student(string nm) {
-		cout << "calling canonical constructor with parameters 2" << endl;
+		cout << "calling constructor with parameters 1" << endl;
 		name = nm;
 		age = 0;
 		mark = 0;
+	}
+	Student(double m) {
+		cout << "calling constructor with parameters 2" << endl;
+		name = "no name";
+		age = 0;
+		mark = m;
+	}
+
+	// destructor (деструктор)
+
+	~Student() {
+		cout << "calling destructor" << endl;
+		//delete[] mark;
 	}
 
 	string convert() {
