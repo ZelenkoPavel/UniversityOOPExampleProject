47cb6980d45d5349228121fe6db9b6d5634b6cc9
ITSTEP\st <st>
2024-04-02 18:42:50 +0300

    add new class "Manager"

---
+++ Project/Manager.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "Student.h"
+
+
+class Manager {
+public:
+	double getMaxMark(Student* students, int length) {
+		double max = students[0].mark;
+
+		for (int i = 1; i < length; i++)
+		{
+			if (max < students[i].mark) {
+				max = students[i].mark;
+			}
+		}
+
+		return max;
+	}
+	double getMinMark(Student* students, int length) {
+		double min = students[0].mark;
+
+		for (int i = 1; i < length; i++)
+		{
+			if (min > students[i].mark) {
+				min = students[i].mark;
+			}
+		}
+
+		return min;
+	}
+	double calcAverageMark(Student* students, int length) {
+		double average = 0;
+
+		for (int i = 0; i < length; i++)
+		{
+			average += students[i].mark;
+		}
+
+		return average / length;
+	}
+
+	Student getBestStudent(Student* students, int length) {
+		double bestMark = getMaxMark(students, length);
+		
+		Student st = NULL;
+
+		for (int j = 0; j < length; j++)
+		{
+			if (bestMark == students[j].mark) {
+				st = students[j];
+			}
+		}
+		return st;
+	}
+
+};
