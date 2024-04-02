469a911a15174440e63649dd37687b871ce5bede
ITSTEP\st <st>
2024-04-02 18:52:19 +0300

    .

--- Project/Student.h
+++ Project/Student.h
@@ -14,7 +14,7 @@ public:
 
 	// default constructor
 	Student() {
-		cout << "calling default constructor" << endl;
+		//cout << "calling default constructor" << endl;
 		name = "no name";
 		age = 5;
 		mark = 4;
@@ -24,20 +24,20 @@ public:
 
 	// canonical constructor with parameters (arguments)
 	Student(string nm, int a, double m) {
-		cout << "calling canonical constructor" << endl;
+		//cout << "calling canonical constructor" << endl;
 		name = nm;
 		age = a;
 		mark = m;
 	}
 
 	Student(string nm) {
-		cout << "calling constructor with parameters 1" << endl;
+		//cout << "calling constructor with parameters 1" << endl;
 		name = nm;
 		age = 0;
 		mark = 0;
 	}
 	Student(double m) {
-		cout << "calling constructor with parameters 2" << endl;
+		//cout << "calling constructor with parameters 2" << endl;
 		name = "no name";
 		age = 0;
 		mark = m;
@@ -50,7 +50,7 @@ public:
 
 	// destructor (деструктор)
 	~Student() {
-		cout << "calling destructor" << endl;
+		//cout << "calling destructor" << endl;
 		//delete[] mark;
 	}
 
