5c33f97c81bcc552b056a3244138f73e3e04b697
ITSTEP\st <st>
2024-04-25 18:43:55 +0300

    add stack

---
+++ ProjectADS/Stack.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Stack
+{
+private:
+	int* stack;
+	int size{ 0 };
+
+public:
+	void push(int element);
+	int pop();
+	int peek();
+	int getSize();
+	bool isEmpty();
+	string getInfo();
+};
+
+
