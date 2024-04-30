327affac4ccdb5ef75d0f94e96cd37699f379e7f
ITSTEP\st <st>
2024-04-30 18:23:13 +0300

    add new class Worker

---
+++ Project/Worker.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Worker
+{
+private:
+	string name;
+	int age;
+	double salary;
+
+public:
+	Worker() {}
+	Worker(string name, int age, double salary) :
+		name(name), age(age), salary(salary) {}
+
+	~Worker() {}
+
+	string getName();
+	void setName(string name);
+	int getAge();
+	void setAge(int age);
+	double getSalary();
+	double setSalary(double salary);
+
+	string getInfo();
+};
+
