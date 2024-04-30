80c5893f4bc936d2411f33ba61e727bdf4e65d98
ITSTEP\st <st>
2024-04-30 18:36:11 +0300

    Add base class Human

---
+++ Project/Human.cpp
@@ -0,0 +1,24 @@
+#include "Human.h"
+
+
+string Human::getName() {
+	return name;
+}
+void Human::setName(string name) {
+	this->name = name;
+}
+int Human::getAge() {
+	return age;
+}
+void Human::setAge(int age) {
+	if (age > 0 && age < 90) {
+		this->age = age;
+	}
+}
+
+string Human::getInfo() {
+	string s = "";
+
+	s += name + ": age - ";
+	s += to_string(age);
+}
