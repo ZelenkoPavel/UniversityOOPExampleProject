78e9461d9020fc625c3f3859da50a02163535242
Зеленко Павел <157722346+ZelenkoPavel@users.noreply.github.com>
2024-05-08 20:10:32 +0300

    testing class Integer

--- IntegerProject/main.cpp
+++ IntegerProject/main.cpp
@@ -14,6 +14,13 @@ int main() {
 	Integer integer8(integer1.mod(integer2));
 	Integer integer9(integer1.opp());
 
+	cout << "add - " << integer3.getValue() << endl;
+	cout << "sub - " << integer4.getValue() << endl;
+	cout << "mul - " << integer5.getValue() << endl;
+	cout << "div - " << integer6.getValue() << endl;
+	cout << "power - " << integer7.getValue() << endl;
+	cout << "mod - " << integer8.getValue() << endl;
+	cout << "opp - " << integer9.getValue() << endl;
 
 	cout << "" << endl;
 	
