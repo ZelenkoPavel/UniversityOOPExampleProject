56254257592631dbc880eb1a1399756a0697c085
tokio220506.pavel@gmail.com <tokio220506.pavel@gmail.com>
2024-06-10 15:12:27 +0300

    ExtraSpacesTask09 "not resolved"

---
+++ FileExampleProject/ExtraSpacesTask09.cpp
@@ -0,0 +1,37 @@
+#include <iostream>	// input-output streams
+#include <fstream>	// file streams
+#include <string>
+
+using namespace std;
+
+int main() {
+	string msg;
+	
+	ifstream fin("input.txt");
+	ofstream fout("output.txt");
+	
+	ofstream fout1("output1.txt");
+	ifstream fin1("output1.txt");
+
+	while (!fin.eof()) {
+		
+		getline(fin, msg);
+		fout1 << msg << endl;
+
+		while (!fin1.eof()) {
+			string s;
+			fin1 >> s;
+			
+			fout << s << " ";
+		}
+		fout << endl;
+
+	}
+	fin1.close();
+	fout1.close();
+
+	fin.close();
+	fout.close();
+
+	return 0;
+}
--- FileExampleProject/SumTask08.cpp
+++ FileExampleProject/SumTask08.cpp
@@ -1,22 +1,22 @@
-#include <iostream>	// input-output streams
-#include <fstream>	// file streams
-using namespace std;
-
-int main() {
-	int a, b;
-
-	ifstream fin;
-	fin.open("input.txt");
-
-	fin >> a >> b;
-
-	fin.close();
-	ofstream fout;
-	fout.open("output.txt");
-
-	fout << a << " + " << b << " = " << (a + b) << endl;
-
-	fout.close();
-
-	return 0;
-}
+//#include <iostream>	// input-output streams
+//#include <fstream>	// file streams
+//using namespace std;
+//
+//int main() {
+//	int a, b;
+//
+//	ifstream fin;
+//	fin.open("input.txt");
+//
+//	fin >> a >> b;
+//
+//	fin.close();
+//	ofstream fout;
+//	fout.open("output.txt");
+//
+//	fout << a << " + " << b << " = " << (a + b) << endl;
+//
+//	fout.close();
+//
+//	return 0;
+//}
